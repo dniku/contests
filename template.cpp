aadca0281a794f6972262b0287d129a129898f27
Pastafarianist <mr.pastafarianist@gmail.com>
2015-10-03 23:35:52 +0300

    Added C++ template

---
+++ template.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+typedef long long int ll;
+
+int main() {
+#ifndef ONLINE_JUDGE
+    std::string fncpp = __FILE__;
+    std::string fnin = fncpp.substr(0, fncpp.find_last_of(".")) + ".in"; 
+    freopen(fnin.c_str(), "r", stdin);
+#endif
+
+    return 0;
+}
