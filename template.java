09c3ae36d78a8d387dd8914459e87e4fb6d89815
Pastafarianist <mr.pastafarianist@gmail.com>
2016-09-11 11:52:10 +0100

    Major update
    - Replaced tabs with spaces (default in Intellij)
    - Got rid of the Thread (why was it there in the first place?)
    - Moved solve() to the top
    - Got rid of the debugging function at the bottom I never used anyway
    - Replaced getName() with getSimpleName(), since it does not include the package name

--- template.java
+++ template.java
@@ -1,67 +1,56 @@
 import java.io.*;
 import java.util.*;
 
-public class template implements Runnable {
-	public static final String taskname = template.class.getName();
-	
-	public StringTokenizer strtok;
-	public BufferedReader inr;
-	public PrintWriter out;
+public class template {
+    private void solve() throws NumberFormatException, IOException {
+        // TODO: write solution
+    }
+    
+    // template below this point
+    
+    private static final String taskname = template.class.getSimpleName();
 
-	public static void main(String[] args) {
-		new Thread(new template()).start();
-	}
+    private StringTokenizer strtok;
+    private BufferedReader inr;
+    private PrintWriter out;
 
-	public void run() {
-		Locale.setDefault(Locale.US);
-		boolean oj = System.getProperty("ONLINE_JUDGE") != null;
-		try {
-			inr = new BufferedReader(oj ? new InputStreamReader(System.in, "ISO-8859-1") : new FileReader(taskname + ".in"));
-			out = new PrintWriter(oj ? new OutputStreamWriter(System.out, "ISO-8859-1") : new FileWriter(taskname + ".out"));
-			solve();
-		} catch (Exception e) {
-			if (!oj)
-				e.printStackTrace();
-			System.exit(9000);
-		} finally {
-			out.flush();
-			out.close();
-		}
-	}
+    public static void main(String[] args) {
+        (new template()).run();
+    }
 
-	public String nextToken() throws IOException {
-		while (strtok == null || !strtok.hasMoreTokens()) {
-			strtok = new StringTokenizer(inr.readLine());
-		}
-		return strtok.nextToken();
-	}
+    private void run() {
+        Locale.setDefault(Locale.US);
+        boolean oj = System.getProperty("ONLINE_JUDGE") != null;
+        try {
+            inr = new BufferedReader(oj ? new InputStreamReader(System.in, "ISO-8859-1") : new FileReader(taskname + ".in"));
+            out = new PrintWriter(oj ? new OutputStreamWriter(System.out, "ISO-8859-1") : new FileWriter(taskname + ".out"));
+            solve();
+        } catch (Exception e) {
+            if (!oj)
+                e.printStackTrace();
+            System.exit(9000);
+        } finally {
+            out.flush();
+            out.close();
+        }
+    }
 
-	public int nextInt() throws NumberFormatException, IOException {
-		return Integer.parseInt(nextToken());
-	}
+    private String nextToken() throws IOException {
+        while (strtok == null || !strtok.hasMoreTokens()) {
+            strtok = new StringTokenizer(inr.readLine());
+        }
+        return strtok.nextToken();
+    }
 
-	public long nextLong() throws NumberFormatException, IOException {
-		return Long.parseLong(nextToken());
-	}
+    private int nextInt() throws NumberFormatException, IOException {
+        return Integer.parseInt(nextToken());
+    }
 
-	public double nextDouble() throws NumberFormatException, IOException {
-		return Double.parseDouble(nextToken());
-	}
+    private long nextLong() throws NumberFormatException, IOException {
+        return Long.parseLong(nextToken());
+    }
 
-	// Debugging
-
-	public static void printArray(boolean[][] a) {
-		for (int i = 0; i < a.length; i++) {
-			for (int j = 0; j < a[i].length; j++) {
-				System.out.print(a[i][j] ? '#' : '.');
-			}
-			System.out.print('\n');
-		}
-	}
-
-	// Solution
-
-	public void solve() throws NumberFormatException, IOException {
-		// TODO: write solution
-	}
+    private double nextDouble() throws NumberFormatException, IOException {
+        return Double.parseDouble(nextToken());
+    }
 }
