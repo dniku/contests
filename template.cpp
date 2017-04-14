caae3eeffaab81fc60e676ef3013b7a4f9c777e7
Dmitry Nikulin <Pastafarianist@users.noreply.github.com>
2017-04-14 16:53:54 +0300

    Update template.cpp

--- template.cpp
+++ template.cpp
@@ -1,12 +1,48 @@
+#define _USE_MATH_DEFINES
 #include <bits/stdc++.h>
-typedef long long int ll;
+using namespace std;
+
+#define eprintf(...) fprintf(stderr, __VA_ARGS__)
+#define forn(i, n) for (size_t i = 0; i < (n); ++i)
+void timestamp(char const * const tag, bool absolute = false);
+
+// </template>
+
+void read() {
+
+}
+
+void solve() {
+
+}
+
+// <template>
+
+void timestamp(char const * const tag, bool absolute) {
+  static double last = 0;
+  double now = (static_cast<double>(clock()) / CLOCKS_PER_SEC) * 1000;
+  if (absolute) {
+    eprintf("%s: %.2lf ms\n", tag, now);
+  }
+  else {
+    eprintf("%s: %.2lf ms\n", tag, now - last);
+    last = now;
+  }
+}
 
 int main() {
-#ifndef ONLINE_JUDGE
-    std::string fncpp = __FILE__;
-    std::string fnin = fncpp.substr(0, fncpp.find_last_of(".")) + ".in"; 
-    freopen(fnin.c_str(), "r", stdin);
+  timestamp("start");
+#ifdef DEV
+  string taskname = "";
+  string fnin = taskname + ".in";
+  assert(freopen(fnin.c_str(), "r", stdin));
+  timestamp("freopen()");
 #endif
-
-    return 0;
+  
+  read();
+  timestamp("read()");
+  solve();
+  timestamp("solve()");
+  timestamp("total", true);
+  return 0;
 }
